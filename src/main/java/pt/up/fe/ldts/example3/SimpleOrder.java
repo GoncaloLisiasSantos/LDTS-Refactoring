14de736307c883a42f3dfe52445a73b1d6121607
Guca08 <up202108839@up.pt>
2022-12-18 21:04:11 +0000

    example 3 with null disc

--- src/main/java/pt/up/fe/ldts/example3/SimpleOrder.java
+++ src/main/java/pt/up/fe/ldts/example3/SimpleOrder.java
@@ -2,10 +2,12 @@ package pt.up.fe.ldts.example3;
 
 public class SimpleOrder {
     private Discount discount;
-    private double price;
+    private final double price;
 
     public SimpleOrder(double price) {
+
         this.price = price;
+        discount = new noDiscount();
     }
 
     public void setDiscount(Discount discount) {
@@ -13,9 +15,6 @@ public class SimpleOrder {
     }
 
     public double getTotal() {
-        if (discount == null)
-            return price;
-        else
-            return discount.applyDiscount(price);
+        return discount.applyDiscount(price);
     }
 }
